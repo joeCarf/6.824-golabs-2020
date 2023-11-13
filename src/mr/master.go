2d1fca827305dcf7f603bb29c3bdbf424a30ddc7
Ao Qiao <qiao_ao@foxmail.com>
2023-11-13 11:29:09 +0800

    Lab1 implementation (#3)
    * 定义Master/Worker及RPC
    
    * 定义Master/Worker及RPC
    
    * finish ask for MappingTask
    
    * finish Mapping phase
    
    * finish Reduce phase and past all tests
    
    * Doc & pic
    
    * Doc

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,16 +1,71 @@
 package mr
 
-import "log"
+import (
+	"log"
+	"strconv"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
-
 type Master struct {
 	// Your definitions here.
+	WorkingStatus      Status       // Master所处工作状态
+	FileNames          []string     // 执行任务的文件名
+	MapWorkingQueue    WorkingQueue // 正在进行的Map任务队列
+	ReduceWorkingQueue WorkingQueue // 正在进行的Reduce任务队列
+	CurrentTaskId      int32        // 当前正在进行的任务Id
+	TaskIdMutex        sync.Mutex   // 任务id自增时锁
+	MapTaskChannel     chan *Task   // 存放Map任务的channel
+	ReduceTaskChannel  chan *Task   // 存放Reduce任务的channel
 
+	NReduce int // 划分成多少个reduce任务
 }
+
+type WorkingQueue struct {
+	Queue map[int]*Task
+	mu    sync.Mutex
+}
+
+func (wq *WorkingQueue) removeTaskById(taskId int) {
+	wq.mu.Lock()
+	delete(wq.Queue, taskId)
+	wq.mu.Unlock()
+}
+
+func (wq *WorkingQueue) putTask(taskId int, task Task) {
+	wq.mu.Lock()
+	wq.Queue[taskId] = &task
+	wq.mu.Unlock()
+}
+
+type Status = int32
+
+const (
+	Mapping = iota
+	Reducing
+	Done
+)
+
+type Task struct {
+	StartTime int64    // 开始执行时间戳
+	FileName  string   // 任务对应的文件名
+	TaskId    int32    // 任务号
+	Type      TaskType // 任务类型
+	NReduce   int      // reduce任务个数（用来hash）
+}
+
+type TaskType = int32
+
+const (
+	MapTask = iota
+	ReduceTask
+	None
+	Sleep
+)
 
 // Your code here -- RPC handlers for the worker to call.
 
@@ -24,6 +79,106 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 
+/**
+ * 下面是暴露给worker的RPC接口
+ */
+func (m *Master) AskForTask(request *AskForTaskRequest, response *AskForTaskResponse) error {
+	var task Task
+	switch m.WorkingStatus {
+	case Mapping:
+		task, _ = m.processMappingTask()
+	case Reducing:
+		task, _ = m.processReducingTask()
+	case Done:
+		task = Task{Type: None}
+	}
+
+	response.Task = task
+	//fmt.Println("the returned task is: ", response.Task)
+	return nil
+}
+
+func (m *Master) NotifyTaskDone(
+	request *NotifyTaskDoneRequest,
+	response *NotifyTaskDoneResponse) error {
+
+	if request.TaskType == MapTask {
+		m.MapWorkingQueue.removeTaskById(request.TaskId)
+	} else {
+		m.ReduceWorkingQueue.removeTaskById(request.TaskId)
+	}
+
+	if m.WorkingStatus == Mapping && len(m.MapTaskChannel) == 0 && len(m.MapWorkingQueue.Queue) == 0 {
+		m.WorkingStatus = Reducing
+		m.prepareReducingTask()
+	} else if m.WorkingStatus == Reducing && len(m.ReduceTaskChannel) == 0 && len(m.ReduceWorkingQueue.Queue) == 0 {
+		m.WorkingStatus = Done
+	}
+
+	return nil
+}
+
+func (m *Master) prepareReducingTask() {
+	m.TaskIdMutex.Lock()
+	defer m.TaskIdMutex.Unlock()
+	if len(m.ReduceTaskChannel) > 0 {
+		return
+	}
+
+	for i := 0; i < m.NReduce; i++ {
+		var task = Task{FileName: strconv.Itoa(int(m.CurrentTaskId)), TaskId: int32(i)}
+		m.ReduceTaskChannel <- &task
+	}
+}
+
+func (m *Master) processMappingTask() (Task, error) {
+	//fmt.Println("receive asked for mapping task from worker...")
+	if len(m.MapTaskChannel) == 0 {
+		return Task{Type: Sleep}, nil
+	}
+	task := <-m.MapTaskChannel
+	if (*task).TaskId == -1 && (*task).StartTime == -1 {
+		(*task).TaskId = m.generateTaskId()
+	}
+	(*task).StartTime = m.generateStartTime()
+	(*task).Type = MapTask
+	(*task).NReduce = m.NReduce
+
+	m.MapWorkingQueue.putTask(int((*task).TaskId), *task)
+	//fmt.Println("send back map task...", task)
+	return *task, nil
+}
+
+func (m *Master) generateTaskId() int32 {
+	var taskId int32
+
+	m.TaskIdMutex.Lock()
+	taskId = m.CurrentTaskId
+	m.CurrentTaskId++
+	m.TaskIdMutex.Unlock()
+
+	return taskId
+}
+
+func (m *Master) generateStartTime() int64 {
+	now := time.Now()
+	return now.Unix()
+}
+
+func (m *Master) processReducingTask() (Task, error) {
+	//fmt.Println("receive asked for reducing task from worker...")
+	if len(m.ReduceTaskChannel) == 0 {
+		return Task{Type: Sleep}, nil
+	}
+	task := <-m.ReduceTaskChannel
+	(*task).StartTime = m.generateStartTime()
+	(*task).Type = ReduceTask
+	(*task).NReduce = m.NReduce
+
+	m.ReduceWorkingQueue.putTask(int((*task).TaskId), *task)
+	//fmt.Println("send back reduce task...", task)
+	return *task, nil
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -49,9 +204,47 @@ func (m *Master) Done() bool {
 	ret := false
 
 	// Your code here.
-
-
+	switch m.WorkingStatus {
+	case Done:
+		ret = true
+	default:
+		ret = false
+	}
 	return ret
+}
+
+func (m *Master) checkTaskList() {
+	for {
+		if m.WorkingStatus == Mapping {
+			if len(m.MapWorkingQueue.Queue) > 0 {
+				for _, task := range m.MapWorkingQueue.Queue {
+					cur := m.generateStartTime()
+					if cur-task.StartTime >= 10 {
+						//fmt.Println("[REPUT] taskid = ", task.TaskId, "  time: ", cur)
+						m.MapTaskChannel <- task
+						task.StartTime = cur
+					}
+				}
+			}
+
+		}
+		if m.WorkingStatus == Reducing {
+			if len(m.ReduceWorkingQueue.Queue) > 0 {
+				for _, task := range m.ReduceWorkingQueue.Queue {
+					cur := m.generateStartTime()
+					if cur-task.StartTime >= 10 {
+						m.ReduceTaskChannel <- task
+						//m.ReduceWorkingQueue.removeTaskById(id)
+						task.StartTime = cur
+					}
+				}
+			}
+		}
+		if m.WorkingStatus == Done {
+			return
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
 }
 
 //
@@ -60,10 +253,30 @@ func (m *Master) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
+	var nMap = len(files) // Map任务个数
+	m := Master{
+		WorkingStatus: Mapping,
+		FileNames:     files,
+		MapWorkingQueue: WorkingQueue{
+			Queue: make(map[int]*Task),
+		},
+		ReduceWorkingQueue: WorkingQueue{
+			Queue: make(map[int]*Task),
+		},
+		CurrentTaskId:     0,
+		MapTaskChannel:    make(chan *Task, nMap),
+		ReduceTaskChannel: make(chan *Task, nReduce),
+		NReduce:           nReduce,
+	}
+
+	for _, file := range files {
+		var task = Task{FileName: file, TaskId: -1, StartTime: -1}
+		m.MapTaskChannel <- &task
+	}
+
+	go m.checkTaskList()
 
 	// Your code here.
-
 
 	m.server()
 	return &m
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,18 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
-
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,6 +21,30 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+type AskForTaskRequest struct {
+}
+
+type AskForTaskResponse struct {
+	Task Task
+}
+
+type NotifyTaskDoneRequest struct {
+	TaskId   int
+	TaskType TaskType
+}
+
+type NotifyTaskDoneResponse struct {
+}
+
+const intermediateFilePrefix string = "mr-in-"
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,6 +56,10 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func generateStartTime() int64 {
+	now := time.Now()
+	return now.Unix()
+}
 
 //
 // main/mrworker.go calls this function.
@@ -32,10 +68,133 @@ func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	for {
+		request := AskForTaskRequest{}
+		response := AskForTaskResponse{}
+		ok := call("Master.AskForTask", &request, &response)
+		if !ok {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		switch response.Task.Type {
+		case MapTask:
+			doMappingTask(mapf, &(response.Task))
+			sendTaskDoneMessage(int(response.Task.TaskId), MapTask)
+		case ReduceTask:
+			doReducingTask(reducef, &(response.Task))
+			sendTaskDoneMessage(int(response.Task.TaskId), ReduceTask)
+		case Sleep:
+			time.Sleep(500 * time.Millisecond)
+		case None:
+			return
+		}
+	}
 
 	// uncomment to send the Example RPC to the master.
 	// CallExample()
 
+}
+
+func doMappingTask(mapf func(string, string) []KeyValue, task *Task) {
+	//fmt.Println("[MAP-Do-]", task.TaskId, " filename=", task.FileName, "======", os.Getpid(), "  time: ", generateStartTime())
+	intermediate := []KeyValue{}
+	filename := task.FileName
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+	intermediate = append(intermediate, kva...)
+
+	HashedKV := make([][]KeyValue, task.NReduce)
+
+	for _, kv := range intermediate {
+		index := ihash(kv.Key) % task.NReduce
+		HashedKV[index] = append(HashedKV[index], kv)
+	}
+
+	for i := 0; i < task.NReduce; i++ {
+		intermediateFileName := intermediateFilePrefix + strconv.Itoa(int(task.TaskId)) + "-" + strconv.Itoa(i)
+		infile, _ := os.Create(intermediateFileName)
+		enc := json.NewEncoder(infile)
+		for _, kv := range HashedKV[i] {
+			err := enc.Encode(&kv)
+			if err != nil {
+				log.Fatalf("err when encoding file %v", intermediateFileName)
+			}
+		}
+	}
+	//fmt.Println("[MAP] map task done...filename=", task.FileName, "  taskid=", task.TaskId, "======", os.Getpid(), "  time: ", generateStartTime())
+
+}
+
+func doReducingTask(reducef func(string, []string) string, task *Task) {
+	//fmt.Println("[REDUCING] doing reduce task...", task)
+
+	filenames := generateReducingFileList(task.FileName, task.TaskId)
+	intermediate := []KeyValue{}
+	for _, filename := range filenames {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+	}
+	sort.Sort(ByKey(intermediate))
+	var outputFileName string = "mr-out-" + strconv.Itoa(int(task.TaskId))
+	var outputContent string
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		//fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+		outputContent += fmt.Sprintf("%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+
+	if err := writeAtomic(outputFileName, []byte(outputContent)); err != nil {
+		log.Fatalln("worker.WorkReduce: writeAtomic failed!")
+	}
+
+	//fmt.Println("[REDUCING] reduce task done...", task)
+}
+
+func generateReducingFileList(maxMapTaskIdStr string, id int32) []string {
+	var filenameList []string
+	maxMapTaskId, _ := strconv.Atoi(maxMapTaskIdStr)
+	for i := 0; i < maxMapTaskId; i++ {
+		filename := intermediateFilePrefix + strconv.Itoa(i) + "-" + strconv.Itoa(int(id))
+		filenameList = append(filenameList, filename)
+	}
+	return filenameList
+}
+
+func sendTaskDoneMessage(taskId int, taskType TaskType) {
+	request := NotifyTaskDoneRequest{TaskId: taskId, TaskType: taskType}
+	response := NotifyTaskDoneRequest{}
+	call("Master.NotifyTaskDone", &request, &response)
 }
 
 //
@@ -82,4 +241,22 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+func writeAtomic(filename string, data []byte) error {
+	//创建临时文件
+	tempFile, err := ioutil.TempFile(filepath.Dir(filename), "temp")
+	//退出前保证关闭临时文件
+	defer tempFile.Close()
+	if err != nil {
+		log.Fatalln("open temp file error", err)
+		return err
+	}
+	//将数据写入临时文件
+	if _, err := tempFile.Write(data); err != nil {
+		return err
+	}
+	//将临时文件改为需要写入的文件
+	err = os.Rename(tempFile.Name(), filename)
+	return err
 }
